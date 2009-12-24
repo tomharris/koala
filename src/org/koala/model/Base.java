3e7d54716fa026c3d82b8cc5ea4f7b81c3a2066f
Tom Harris <thomas.e.harris@gmail.com>
2009-12-23 23:35:24 -0500

    added ability to fetch auto-increment value to the model base class

--- src/org/koala/model/Base.java
+++ src/org/koala/model/Base.java
@@ -8,10 +8,16 @@ package org.koala.model;
  *
  */
 
+import java.sql.*;
+import org.apache.log4j.Logger;
+
+import org.koala.DatabaseConnection;
 import org.koala.exception.EntryAlreadyExistsException;
 
 abstract public class Base {
   protected int id; //non-neg if valid
+
+  private static Logger logger = Logger.getLogger(Base.class);
 
   public Base() {
     this.id = 0;
@@ -33,6 +39,13 @@ abstract public class Base {
     return this.getId() <= 0;
   }
 
+  protected String getTableName() {
+    String class_name = this.getClass().getSimpleName();
+    class_name = underscore(class_name);
+    class_name = pluralize(class_name);
+    return class_name;
+  }
+
   protected void create() throws EntryAlreadyExistsException {
   }
 
@@ -46,5 +59,51 @@ abstract public class Base {
     else {
       this.update();
     }
+  }
+
+  protected static int getAutoIncKey(Base model) {
+    Statement stmt;
+    ResultSet rs;
+    int autoIncKey = -1;
+
+    try {
+      stmt = DatabaseConnection.getInstance().getConnection().createStatement();
+      rs = stmt.executeQuery(DatabaseConnection.getInstance().getProfile().getAutoIncCmd(model.getTableName(), "id"));
+      if (rs.next()) {
+        autoIncKey = rs.getInt(1);
+      }
+      else {
+        throw new SQLException("Getting auto inc key failed");
+      }
+
+      rs.close();
+      stmt.close();
+
+      if(autoIncKey == -1) {
+        throw new SQLException("Auto increment key is -1!");
+      }
+    }
+    catch (SQLException e) {
+      logger.error("SQL getting auto increment key", e);
+    }
+
+    return autoIncKey;
+  }
+
+  protected static String underscore(String text) {
+    return text.replaceAll("([A-Z])", "_$1").replaceFirst("^_", "").toLowerCase(); //tablize the name
+  }
+
+  protected static String pluralize(String text) {
+    if (text.matches(".*y$")) {
+      text = text.replaceFirst("y$", "ies");
+    }
+    else if (text.matches(".*ox$")) {
+      text = text.concat("es");
+    }
+    else {
+      text = text.concat("s");
+    }
+    return text;
   }
 }

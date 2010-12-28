f585a86a62cd6aefa1d0a48ee69983894cbf11f4
Tom Harris <thomas.e.harris@gmail.com>
2010-12-28 11:13:37 -0500

    re-dump the schema from 5.1 to replace the version created with 5.0

--- resources/pos_setup_mysql.sql
+++ resources/pos_setup_mysql.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.10
+-- MySQL dump 10.13  Distrib 5.1.53, for apple-darwin10.5.0 (i386)
 --
 -- Host: localhost    Database: pos
 -- ------------------------------------------------------
--- Server version	5.0.26-log
+-- Server version	5.1.50
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -24,48 +24,104 @@ USE `pos`;
 --
 
 DROP TABLE IF EXISTS `customers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `customers` (
-  `id` int(10) unsigned NOT NULL auto_increment,
-  `balance` decimal(10,2) NOT NULL default '0.00',
-  `firstname` varchar(32) NOT NULL default '',
-  `lastname` varchar(32) NOT NULL default '',
-  `comp` tinyint(3) unsigned NOT NULL default '0',
-  `renewamount` decimal(10,2) NOT NULL default '0.00',
-  `note` varchar(255) NOT NULL default '',
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `balance` decimal(10,2) NOT NULL DEFAULT '0.00',
+  `firstname` varchar(32) NOT NULL DEFAULT '',
+  `lastname` varchar(32) NOT NULL DEFAULT '',
+  `comp` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `renewamount` decimal(10,2) NOT NULL DEFAULT '0.00',
+  `note` varchar(255) NOT NULL DEFAULT '',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `inventory`
 --
 
 DROP TABLE IF EXISTS `inventory`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `inventory` (
-  `id` int(10) unsigned NOT NULL auto_increment,
-  `sku` varchar(32) NOT NULL default '',
-  `quantity` int(10) unsigned NOT NULL default '0',
-  `name` varchar(128) NOT NULL default '',
-  `price` decimal(10,2) NOT NULL default '0.00',
-  `tax_rate` decimal(3,2) NOT NULL default '0.00',
-  `rentable` tinyint(3) unsigned NOT NULL default '0',
-  `unlimited` tinyint(3) unsigned NOT NULL default '0',
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `sku` varchar(32) NOT NULL DEFAULT '',
+  `quantity` int(10) unsigned NOT NULL DEFAULT '0',
+  `name` varchar(128) NOT NULL DEFAULT '',
+  `price` decimal(10,2) NOT NULL DEFAULT '0.00',
+  `tax_rate` decimal(3,2) NOT NULL DEFAULT '0.00',
+  `rentable` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `unlimited` tinyint(3) unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `transaction_items`
+--
+
+DROP TABLE IF EXISTS `transaction_items`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `transaction_items` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `transaction_id` int(10) unsigned NOT NULL,
+  `sku` varchar(32) NOT NULL,
+  `name` varchar(128) NOT NULL DEFAULT '',
+  `quantity` int(10) unsigned NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `tax_rate` decimal(3,2) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `transaction_items_transaction_id_fkey` (`transaction_id`),
+  CONSTRAINT `transaction_items_transaction_id_fkey` FOREIGN KEY (`transaction_id`) REFERENCES `transactions` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `transactions`
+--
+
+DROP TABLE IF EXISTS `transactions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `transactions` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `transaction_time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `code` char(1) NOT NULL DEFAULT '',
+  `user_id` int(10) unsigned NOT NULL,
+  `customer_id` int(10) unsigned DEFAULT NULL,
+  `account_subtotal` decimal(10,2) NOT NULL DEFAULT '0.00',
+  `account_taxtotal` decimal(10,2) NOT NULL DEFAULT '0.00',
+  `subtotal` decimal(10,2) NOT NULL DEFAULT '0.00',
+  `taxtotal` decimal(10,2) NOT NULL DEFAULT '0.00',
+  PRIMARY KEY (`id`),
+  KEY `transactions_cashierid_fkey` (`user_id`),
+  KEY `transactions_customerid_fkey` (`customer_id`),
+  CONSTRAINT `transactions_cashierid_fkey` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  CONSTRAINT `transactions_customerid_fkey` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `users`
 --
 
 DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `users` (
-  `id` int(10) unsigned NOT NULL auto_increment,
-  `username` varchar(32) NOT NULL default '',
-  `password_hash` varbinary(41) NOT NULL default '',
-  `level` tinyint(4) NOT NULL default '0',
-  `firstname` varchar(32) NOT NULL default '',
-  `lastname` varchar(32) NOT NULL default '',
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(32) NOT NULL DEFAULT '',
+  `password_hash` varbinary(41) NOT NULL DEFAULT '',
+  `level` tinyint(4) NOT NULL DEFAULT '0',
+  `firstname` varchar(32) NOT NULL DEFAULT '',
+  `lastname` varchar(32) NOT NULL DEFAULT '',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 --
 -- Dumping data for table `users`
@@ -77,47 +133,6 @@ INSERT INTO `users` VALUES (1,'admin',MD5('pos'),3,'pos','admin');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 
---
--- Table structure for table `transactions`
---
-
-DROP TABLE IF EXISTS `transactions`;
-CREATE TABLE `transactions` (
-  `id` int(10) unsigned NOT NULL auto_increment,
-  `transaction_time` datetime NOT NULL default '0000-00-00 00:00:00',
-  `code` char(1) NOT NULL default '',
-  `user_id` int(10) unsigned NOT NULL,
-  `customer_id` int(10) unsigned,
-  `account_subtotal` decimal(10,2) NOT NULL default '0.00',
-  `account_taxtotal` decimal(10,2) NOT NULL default '0.00',
-  `subtotal` decimal(10,2) NOT NULL default '0.00',
-  `taxtotal` decimal(10,2) NOT NULL default '0.00',
-  PRIMARY KEY (`id`),
-  KEY `transactions_cashierid_fkey` (`user_id`),
-  CONSTRAINT `transactions_cashierid_fkey` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
-  KEY `transactions_customerid_fkey` (`customer_id`),
-  CONSTRAINT `transactions_customerid_fkey` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Table structure for table `transaction_items`
---
-
-DROP TABLE IF EXISTS `transaction_items`;
-CREATE TABLE `transaction_items` (
-  `id` int(10) unsigned NOT NULL auto_increment,
-  `transaction_id` int(10) unsigned NOT NULL,
-  `sku` varchar(32) NOT NULL,
-  `name` varchar(128) NOT NULL default '',
-  `quantity` int(10) unsigned NOT NULL,
-  `price` decimal(10,2) NOT NULL,
-  `tax_rate` decimal(3,2) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `transaction_items_transaction_id_fkey` (`transaction_id`),
-  CONSTRAINT `transaction_items_transaction_id_fkey` FOREIGN KEY (`transaction_id`) REFERENCES `transactions` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
 /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
